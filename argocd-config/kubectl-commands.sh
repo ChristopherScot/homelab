ac7f3c0e3d19ce5af1fd0c53d183fcd0b552b0f6
ChristopherScot <16506996+ChristopherScot@users.noreply.github.com>
2024-12-13 22:49:58 -0500

    clusterRole?

--- argocd-config/kubectl-commands.sh
+++ argocd-config/kubectl-commands.sh
@@ -1,2 +1,2 @@
 kubectl create namespace argo-rollouts
-kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
+kubectl apply -n argocd -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
